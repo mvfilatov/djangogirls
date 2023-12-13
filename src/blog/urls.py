2681a5e9b542bf1a6e59a004fab17cbc4dbd8431
mvfilatov <maxim.v.filatov@gmail.com>
2023-12-13 19:51:20 +0300

    Add project tools

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.post_list, name='post_list',)
+    path(
+        '',
+        views.post_list,
+        name='post_list',
+    ),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-def post_list(request):
+
+def post_list(request: HttpRequest) -> HttpResponse:
     return render(request, 'blog/post_list.html', {})
